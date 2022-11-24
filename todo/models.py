54113ccd46721b772ffccdf0939fa10239e95348
JonasRegener <regener.jonas@gmail.com>
2022-11-25 00:13:03 +0100

    added Todo.Model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+import datetime
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+        title = models.CharField(max_length=30)
+        date = models.CharField(max_length=30)
+        created_at = models.DateField(default=datetime.date.today)
+        user = models.ForeignKey(
+            User,
+            on_delete=models.CASCADE
+        )
+        
+        # Create your models here.
