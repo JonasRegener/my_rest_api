5851cff96ceed501562899b7c59b36900e6f4e1e
JonasRegener <regener.jonas@gmail.com>
2022-11-25 18:27:03 +0100

    added user serializer and time passed

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from .models import Todo
+from .models import Todo, User
 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'email'] #  'user'
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
-    
+
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at',] #  'user'
+        fields = ['id', 'title', 'description', 'created_at', 'user', 'time_passed']
+
+
