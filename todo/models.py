7ad8a08521f901c11068a2caa6b2ffc746350a3a
JonasRegener <regener.jonas@gmail.com>
2022-11-25 16:20:46 +0100

    python 3.9 fix

--- todo/models.py
+++ todo/models.py
@@ -4,11 +4,11 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 class Todo(models.Model):
-        title = models.CharField(max_length=30)
-        description = models.CharField(max_length=30)
-        created_at = models.DateField(default=datetime.date.today)
-        user = models.ForeignKey(
-            User,
-            on_delete=models.CASCADE,
-            default=None
-        )
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=30)
+    created_at = models.DateField(default=datetime.date.today)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        default=None
+    )
