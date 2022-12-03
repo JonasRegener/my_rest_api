6a837dbf20ce82ca3d844527281f0efc4430dc73
JonasRegener <regener.jonas@gmail.com>
2022-12-03 22:36:38 +0100

    subtasks now works perfect

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,17 +1,18 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers, fields
 from .models import Todo, User, Subtask
+from drf_writable_nested.serializers import WritableNestedModelSerializer
 
-class SubtaskSerializer(serializers.ModelSerializer):
+class SubtaskSerializer(WritableNestedModelSerializer):
     
     class Meta:
         model = Subtask
         fields = [ 'id', 'title' ] #  'subtasks'
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class TodoSerializer(WritableNestedModelSerializer):
     
     due_date = fields.DateField(input_formats=['%m/%d/%Y'])
-    subtasks = SubtaskSerializer(many=True, read_only=False)
+    subtasks = SubtaskSerializer(many=True) # , read_only=True
     class Meta:
         model = Todo
         fields = ['id', 'title', 'description',  'category', 'priority',  'user','due_date', 'status', 'subtasks'] #  'subtasks'
--- todo/views.py
+++ todo/views.py
@@ -8,26 +8,30 @@ from django.contrib.auth.models import User
 import json
 
 
+
 # Create your views here.
 
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by('-due_date')
     serializer_class = TodoSerializer
     
-    def create(self, request):
+def create(self, request):
+    if request.method == 'GET':
         todo = Todo.objects.create(title = request.POST.get('title',''),
-                                    description = request.POST.get('description', ''), 
-                                    category = request.POST.get('category', 'none'),
-                                    priority = request.POST.get('priority', 'low'),
-                                    user= request.POST.get('user', ''),
-                                    due_date= request.POST.get('due_date','01-01-2000'),
-                                    status= request.POST.get('status', 'To do'),
-                                    )
+        description = request.POST.get('description', ''), 
+        category = request.POST.get('category', 'none'),
+        priority = request.POST.get('priority', 'low'),
+        user= request.POST.get('user', ''),
+        due_date= request.POST.get('due_date','01-01-2000'),
+        status= request.POST.get('status', 'To do'),subtasks= request.POST.get('subtasks', ''),
+        )
         serzialized_obj = serializers.serialize('json', [todo, ])
         return HttpResponse(serzialized_obj, content_type='application/json')
+    if request.method == 'POST':
+        new_todo = Todo.objects.create(title=request.POST['title'], description=request.POST['description'], category=request.POST['category'], priority=request.POST['priority'], user=request.POST['user'],due_date=request.POST['due_date'],status=request.POST['status'], subtasks=request.POST['title'])
+        new_todo.save()
 
-
-    def update(self, instance, validated_data):
+def update(self, instance, validated_data):
         subtasks_data = validated_data.pop('subtasks')
         instance = super(TodoSerializer, self).update(instance, validated_data)
 
